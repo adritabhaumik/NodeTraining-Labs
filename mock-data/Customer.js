3304c52cb1a8a0de97c7ffd478e740f315f64337
adritabhaumik <35094127+adritabhaumik@users.noreply.github.com>
2020-10-15 10:44:31 +0530

    Add files via upload

---
+++ mock-data/Customer.js
@@ -0,0 +1,21 @@
+const CUSTOMERS = [
+    {
+        id: 1,
+        custName: "cust1",
+        address:"delhi"
+
+    },
+    {
+        id: 2,
+        custName: "cust2",
+        address:"Chennai"
+
+    },
+    {
+        id: 3,
+        custName: "cust3",
+        address:"Mumbai"
+
+    },
+];
+module.exports =  CUSTOMERS;
